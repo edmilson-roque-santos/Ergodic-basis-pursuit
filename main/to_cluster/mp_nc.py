f2909a5ebe2cf6f55e2a3fbbe6e9e580bd3b4751
edmilson.roque <edmilson.roque.usp@gmail.com>
2022-12-28 15:31:59 -0300

    refactor: multi process for each script

---
+++ main/to_cluster/mp_nc.py
@@ -0,0 +1,36 @@
+"""
+Script for multiprocessing experiments
+
+Created on Mon Dec 26 11:26:51 2022
+
+@author: Edmilson Roque dos Santos
+"""
+
+
+from multiprocessing import Pool 
+import numpy as np
+import time
+
+import compare_basis_choice as cbc
+
+def realization(ps_pool):
+    cbc.ring_graph_script(ps_pool)
+    print('\t\t Executed seed {} with {}s'.format(ps_pool, time.time()))
+
+##### Randomness
+Nseeds = 10
+MonteCarlo_seeds = np.arange(1, Nseeds + 1)     # Seed for random number generator
+nproc = 10
+ti = time.time()
+ps = range(1, Nseeds + 1)
+    
+pool = Pool(processes=nproc)
+pool.map(realization, ps)
+pool.close()
+
+tf = time.time()
+execution_time = tf - ti
+print('>> total execution time was {:.2f} min, using {:} realizations of the adaptative analysis'.format(execution_time/60, Nseeds))
+
+
+
